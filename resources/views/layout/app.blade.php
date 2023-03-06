d26e9b001e5509459ea18213f61845e5f2bc898b
Marosy Gergo <garry955@gmail.com>
2023-03-06 21:28:54 +0100

    Old layout component deleted
    - Implement Dialog Modal component at app layout
    - Can be accessible via isModalOpen = bool prop

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,30 +1,40 @@
 <!doctype html>
 <html class="dark">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>POST-IT</title>
-  </head>
-  <body class="antialiased relative text-white dark:bg-[#00132b]">
-      {{-- Flash massege to render session messages --}}
-      <x-flash-message />
-      
-      {{-- Navbar section --}}
-      <x-layout.navbar />
+</head>
 
-      {{-- Main wrapper --}}
-      <div class="min-h-screen bg-gray-100 dark:bg-gray-800 md:w-3/6 md:ml-[16.66%] md:mr-auto md:border-r-white md:border-r-2">
-          {{-- Main section --}}
-          {{ $slot }}        
-      </div>
-  
-      {{-- Sidebar --}}
-      <x-layout.sidebar />
-{{-- TODO:SIDEBAR --}}
-      
-          {{-- Import app.js --}}
-          @vite('resources/js/app.js')
-  </body>
+<body class="antialiased relative text-white dark:bg-[#00132b]" x-data="{ 'isModalOpen': false }"
+    x-on:keydown.escape="isModalOpen=false">
+    {{-- Flash Message Component --}}
+    <x-flash-message />
+
+    {{-- Dialog Modal Component --}}
+    <x-dialog-modal />
+
+    {{-- Navbar Component --}}
+    <x-layout.navbar />
+
+    {{-- Main wrapper --}}
+    <div
+        class="min-h-screen bg-gray-100 dark:bg-gray-800 md:w-3/6 md:ml-[16.66%] md:mr-auto md:border-r-white md:border-r-2">
+        {{-- Main section --}}
+        {{ $slot }}
+    </div>
+
+    {{-- Sidebar --}}
+    <x-layout.sidebar />
+    {{-- TODO:SIDEBAR --}}
+
+    {{-- Import app.js --}}
+    @vite('resources/js/app.js')
+</body>
+
 </html>
