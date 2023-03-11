cd95dce0ec77a045ab59d449c3af5dbd4220e7d4
Marosy Gergo <garry955@gmail.com>
2023-03-11 17:34:14 +0100

    Finish formating friend control panel @ user profile

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -2,8 +2,8 @@
     // 'rounded-lg border inline-block transition hover:transition-all hover:ease-in-out hover:duration-500 hover:transform',
     'hover:transition hover:transition-all hover:ease-in-out hover:duration-500 hover:transform',
     'border-none text-green-500 drop-shadow-lg hover:text-slate-300' => $variant === 'link',
-    'border-red w-full rounded border-red-500 border-2 py-1 ml-5 px-5 text-red-500 hover:text-white hover:scale-105 hover:bg-red-500' => $variant === 'red',
-    'border-primary w-full rounded py-1 ml-5 px-5 border-2 border-primary text-primary hover:text-white hover:bg-primary hover:scale-105' => $variant === 'primary'
+    'border-red w-full rounded border-red-500 border-2 text-red-500 hover:text-white hover:scale-105 hover:bg-red-500' => $variant === 'red',
+    'border-primary w-full rounded border-2 border-primary text-primary hover:text-white hover:bg-primary hover:scale-105' => $variant === 'primary'
     // 'py-4 px-6 md:px-9 bg-primary hover:border-primary hover:text-white' => $variant === 'success',
     // 'py-4 px-6 md:px-9 hover:bg-red-700 hover:border-red-700 hover:text-white' => $variant === 'outline-red',
     // 'py-4 px-6 md:px-9 bg-red-700 border-red-700 text-white hover:bg-red-800' => $variant === 'red',
--- resources/views/user/friend-controller.blade.php
+++ resources/views/user/friend-controller.blade.php
@@ -1,30 +1,32 @@
 {{-- Friend Controls Section --}}
+{{-- {{ dd($status) }} --}}
 @if(auth()->user() && auth()->user()->id != $user->id)
-<div class="friend mb-5">
+<div class="friend border-t-2 border-t-primary mt-3 pt-3">
     @switch($status)
         @case('user_requested')
-            <p>{{ $user->name }} has sent you a friend request.</p>
-            <x-button-link href="{{ route('friend.rejectRequest', [$user]) }}" variant="outline-red" class="hover:scale-105">
+            <p class="block mb-4">{{ $user->name }} has sent you a friend request.</p>
+            <x-button-link href="{{ route('friend.rejectRequest', [$user]) }}" variant="red" class="py-2 px-6 mt-4">
                 Reject friend request
             </x-button-link>
-            <x-button-link href="{{ route('friend.acceptRequest', [$user]) }}" variant="success" class="hover:scale-105">
+            <x-button-link href="{{ route('friend.acceptRequest', [$user]) }}" variant="primary" class="py-2 px-6 mt-4 ml-4">
                 Accept friend request
             </x-button-link>
             @break
         @case('my_friend')
-            <p>{{ $user->name }} is your friend since...</p>
-            <x-button-link href="{{ route('friend.unfriend', [$user]) }}" variant="outline-red" class="hover:scale-105">
+            <p class="block mb-4">{{ $user->name }} is your friend since...</p>
+            <x-button-link href="{{ route('friend.unfriend', [$user]) }}" variant="red" class="py-2 px-6 mt-4">
                 Unfriend user
             </x-button-link>
             @break
         @case('friend_requested')
-            <p>You requested {{ $user->name }}</p>
-            <x-button-link href="{{ route('friend.cancelRequest', [$user]) }}" variant="red" class="hover:scale-105">
+            <p class="block mb-4">You requested <b class="text-primary"> {{ $user->name }}</b> as a friend</p>
+            <x-button-link href="{{ route('friend.cancelRequest', [$user]) }}" variant="red" class="py-2 px-6 mt-4">
                 cancel request
             </x-button-link> 
             @break
-        @default
-            <x-button-link href="{{ route('friend.sendRequest', [$user]) }}" variant="success" class="hover:scale-105">
+            @default
+            <p class="block mb-4">Do you know <b class="text-primary"> {{ $user->name }}</b>?</p>
+            <x-button-link href="{{ route('friend.sendRequest', [$user]) }}" variant="primary" class="py-2 px-6">
                 Send friend request
             </x-button-link>
     @endswitch
